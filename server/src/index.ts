fe54da92822e9915afa521db606e5967b6cb446c
Hayate <ha08ya@gmail.com>
2024-03-24 01:47:15 +0900

    feat: :wrench: #7 Sample api

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,14 @@
-console.log('server - hello');
+import express from 'express';
+import type { Express, Request, Response } from 'express';
+
+const app: Express = express();
+const PORT = 8080;
+
+app.get('/allToDos', (req: Request, res: Response) => {
+  return res.send('ToDos');
+});
+
+app.listen(PORT, () => {
+  console.log('server is running 🚀');
+  console.log(`localhost:${PORT}`);
+});
