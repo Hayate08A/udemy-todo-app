95e2d410d13c5f6ab25100c808f56ed105438209
Hayate <ha08ya@gmail.com>
2024-03-27 03:30:01 +0900

    feat: :sparkles: #11 Create todo

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,11 +5,23 @@ import { PrismaClient } from '@prisma/client';
 const app: Express = express();
 const PORT = 8080;
 
+app.use(express.json());
 const prisma = new PrismaClient();
 
 app.get('/allTodos', async (req: Request, res: Response) => {
   const allTodos = await prisma.todo.findMany();
   return res.json(allTodos);
+});
+
+app.post('/createTodo', async (req: Request, res: Response) => {
+  const { title, isCompleted } = req.body;
+  const createTodo = await prisma.todo.create({
+    data: {
+      title,
+      isCompleted,
+    },
+  });
+  return res.json(createTodo);
 });
 
 app.listen(PORT, () => {
