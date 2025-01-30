e2cf2a66374bca5379f324f2531cd7af2d4ef781
h.adachi <hayate.adachi@spacemarket.co.jp>
2025-01-31 03:06:25 +0900

    front - fix: next config

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
+  output: "export", // ✅ `standalone` ではなく `export` にする
   trailingSlash: true, // 末尾のスラッシュを許可
 };
 
